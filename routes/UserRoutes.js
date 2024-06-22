3f1b1a09c4450458126301c3fbe1dd2fba254327
JorgeRoniel <jorge.infor4g@gmail.com>
2024-06-22 15:35:23 -0300

    CRUD completo.

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const route = express.Router();
-const userController = require('../controllers/UserController');
-const db = require("../firebase")
+const db = require('../firebase')
 
 route.get('/', (req, res) => {
     res.render('index', {title: "Principal", mensage: "Hello, world!"});
@@ -10,7 +9,7 @@ route.get('/', (req, res) => {
 route.post('/add', (req, res) => {
     try {
         const data = req.query;
-        const docRef = db.ref('users/').push(data);
+        db.ref('users/').push(data);
         res.status(200).send("User add!");
     } catch (error) {
         res.status(500).send("Erro:"+error);
@@ -25,6 +24,55 @@ route.get('/listusers', (req,res)=>{
         const dt = snapshot.val();
         res.status(200).json(dt);
     })
+});
+
+route.put('/updateuser/:Userid', (req, res) =>{
+    try {
+
+        const id = req.params.Userid;
+        const data = req.query;
+
+        const userRef = db.ref('users/');
+        const snap = userRef.orderByChild('id').equalTo(id).once('value', (snapshot)=>{
+            if(snapshot.exists()){
+                const updates = {};
+                snapshot.forEach(childSnapshot => {
+                    updates[childSnapshot.key] = data;
+                });
+
+                userRef.update(updates);
+                res.status(200).send('User updated!')
+            }else{
+                res.status(404).send("User not found.");
+            }
+        });
+    } catch (error) {
+        res.status(500).send("Error: "+error);
+    }
+});
+
+route.delete('/deleteuser/:Userid', (req, res) => {
+    try {
+        
+        const id = req.params.Userid;
+
+        const userRef = db.ref('users');
+        userRef.orderByChild('id').equalTo(id).once('value', (snapshot) => {
+            if(snapshot.exists()){
+                const d = {};
+                snapshot.forEach(chilSnapshot => {
+                    d[chilSnapshot.key] = null;
+                });
+
+                userRef.update(d);
+                res.status(200).send("User deleted");
+            }else{
+                res.status(404).send('User not found!');
+            }
+        })
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 module.exports = route;
